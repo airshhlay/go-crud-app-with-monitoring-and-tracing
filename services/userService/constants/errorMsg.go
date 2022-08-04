4c9001e84e7578f3fbe56d76fb5c0f414d384f07
Ashleigh Aathar <ashleigh.aathar@shopee.com>
2022-08-04 18:08:49 +0800

    add error constants

---
+++ services/userService/constants/errorMsg.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	ERROR_DATABASE_MSG            = "error_database_operation_failure"
+	ERROR_USER_ALREADY_EXISTS_MSG = "error_user_already_exists"
+	ERROR_PASSWORD_ENCRYPTION_MSG = "error_password_encryption"
+	ERROR_USER_DOES_NOT_EXIST_MSG = "error_user_does_not_exist"
+	ERROR_USER_PASSWORD_MSG       = "error_user_password"
+)
